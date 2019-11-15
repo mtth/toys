8d1c8e0c5b935462d777ae71c90b0df447777477
Matthieu Monsch <matthieu.monsch@gmail.com>
2019-11-14 19:23:08 -0800

    Update Tetris documentation

--- tetris/src/Tetris/Board.hs
+++ tetris/src/Tetris/Board.hs
@@ -1,6 +1,7 @@
 module Tetris.Board (
+  Piece, randomPiece, movePiece, rotatePiece, pieceCoords,
   Board, newBoard, boardCoords, boardWidth,
-  Piece(..), randomPiece, freezePiece, movePiece, rotatePiece, pieceCoords, fitsOn
+  fitsOn, freezePiece
 ) where
 
 import Data.Foldable (toList)
@@ -13,17 +14,22 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import System.Random (randomRIO)
 
+-- | A rectangular Tetris board.
 data Board = Board
   { _boardBox :: YX.Box
   , _boardCoords :: Set YX
   } deriving (Eq, Show)
 
+-- | Returns the coordinates of all populated positions inside the board.
 boardCoords :: Board -> Set YX
 boardCoords = _boardCoords
 
+-- | Returns the number of columns in the board.
 boardWidth :: Board -> Int
 boardWidth = YX.boxWidth . _boardBox
 
+-- | Creates a new board with the given position as extreme point. If at least one of the given
+-- position's fields is negative, the function will return 'Nothing'.
 newBoard :: YX -> Maybe Board
 newBoard extent = case YX.box 0 extent of
   Just box -> Just $ Board box Set.empty
@@ -35,20 +41,28 @@ data Tetromino
 
 type Angle = Int
 
+-- | A Tetris piece.
+--
+-- Pieces are generated via 'randomPiece'.
 data Piece = Piece Angle YX Tetromino deriving (Eq, Show)
 
+-- | Generates a random piece in the given column.
 randomPiece :: Int -> IO Piece
 randomPiece x = do
   a <- randomRIO (0, 3)
   t <- toEnum <$> randomRIO (0, 5)
   pure $ Piece a (YX 0 x) t
 
+-- | Moves a piece by the given 2D offset.
 movePiece :: YX -> Piece -> Piece
 movePiece p0 (Piece a p1 t) = Piece a (p0 + p1) t
 
+-- | Rotates a piece clockwise 90 degrees..
 rotatePiece :: Piece -> Piece
 rotatePiece (Piece a p t) = Piece ((a + 1) `mod` 4) p t
 
+-- | Adds the piece to the board, clearing any newly filled rows. This function returns the number
+-- of cleared rows along with the updated board.
 freezePiece :: Piece -> Board -> (Int, Board)
 freezePiece piece (Board box oldCoords) = (numLines, Board box newCoords) where
   allCoords = oldCoords <> pieceCoords piece
@@ -60,6 +74,8 @@ freezePiece piece (Board box oldCoords) = (numLines, Board box newCoords) where
   newCoords = mconcat $ zipWith toCoords (Map.toDescList nonFullRows) heights
   toCoords (_, xs) y = Set.map (YX y) xs
 
+-- | Checks whether the given piece fits on the board (i.e. within the boundaries of the board and
+-- doesn't overlap with any existing pieces).
 fitsOn :: Piece -> Board -> Bool
 fitsOn piece board =
   let
@@ -67,6 +83,7 @@ fitsOn piece board =
     coords = pieceCoords piece
   in all (\coord -> coord `YX.inBox` box) coords && Set.disjoint coords (_boardCoords board)
 
+-- | Returns the piece's coordinates.
 pieceCoords :: Piece -> Set YX
 pieceCoords = Set.fromList . go where
   go (Piece a p I) =
