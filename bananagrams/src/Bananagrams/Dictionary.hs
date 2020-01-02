8f7b6e25cf67019995ff9a6f2b14807ab20f5698
Matthieu Monsch <matthieu.monsch@gmail.com>
2020-01-01 21:26:02 -0800

    Tighten word matching logic

--- bananagrams/src/Bananagrams/Dictionary.hs
+++ bananagrams/src/Bananagrams/Dictionary.hs
@@ -45,17 +45,17 @@ firstWords :: Dictionary -> Hand -> [Text]
 firstWords (Dictionary items) chars =
   fmap itemTxt $ Vector.toList $ Vector.filter (spellable chars) items
 
--- a..0...b
---  xxxx
-itemOffsets :: Map Int Char -> (Int, Int) -> Item -> [Int]
-itemOffsets chars (leftBound, rightBound) (Item _ vec mset) =
+itemOffsets :: Hand -> Map Int Char -> (Int, Int) -> Item -> [Int]
+itemOffsets hand chars (leftBound, rightBound) (Item _ vec mset) =
   let
     len = Vector.length vec
     minOffset = max 0 (len - 1 - rightBound)
     maxOffset = min (len - 1) leftBound
     matches k = all (\(i, char) -> maybe True (== char) $ vec Vector.!? (i + k)) $ Map.toList chars
     isDisjoint k = Map.notMember (-(k + 1)) chars && Map.notMember (len - k) chars
-    isSpellable n = True -- TODO: Should check that we have enough characters given matches.
+    usedChars k =
+      Multiset.fromList . Map.elems $ Map.filterWithKey (\n _ -> n >= (-k) && n < len - k) chars
+    isSpellable k = mset `Multiset.isSubsetOf` (hand <> usedChars k)
   in filter (\k -> matches k && isDisjoint k && isSpellable k) [minOffset .. maxOffset]
 
 matchingWords :: Dictionary -> Hand -> Map Int Char -> (Int, Int) -> [(Text, Int)]
@@ -63,4 +63,4 @@ matchingWords (Dictionary items) hand chars bounds =
   let
     chars' = foldl' (flip Multiset.insert) hand $ Map.elems chars
     items' = filter (spellable chars') $ Vector.toList items
-  in concat $ fmap (\item -> fmap (itemTxt item,) (itemOffsets chars bounds item)) items'
+  in concat $ fmap (\item -> fmap (itemTxt item,) (itemOffsets hand chars bounds item)) items'
